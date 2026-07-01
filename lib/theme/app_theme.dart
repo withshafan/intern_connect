3af9d05a28c4d1e6e662558ce06b71458ce28e78
MUHAMMAD SHAFAN RIAZ <malikshafan80@gmail.com>
2026-07-01 18:49:42 +0500

    Auto-commit: 2026-07-01 18:49:42

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData light() {
+    final scheme = ColorScheme.fromSeed(seedColor: const Color(0xFF3D5AFE));
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: scheme,
+      scaffoldBackgroundColor: scheme.surface,
+      appBarTheme: AppBarTheme(
+        backgroundColor: scheme.surface,
+        foregroundColor: scheme.onSurface,
+        elevation: 0,
+      ),
+      cardTheme: CardThemeData(
+        elevation: 1,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+        filled: true,
+      ),
+    );
+  }
+}
