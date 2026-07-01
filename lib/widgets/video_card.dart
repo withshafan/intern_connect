4771b23492cc3efd41850cd1fc880fb605846b30
MUHAMMAD SHAFAN RIAZ <malikshafan80@gmail.com>
2026-07-01 21:35:26 +0500

    Auto-commit: 2026-07-01 21:35:25

--- lib/widgets/video_card.dart
+++ lib/widgets/video_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:timeago/timeago.dart' as timeago;
+import 'package:shimmer/shimmer.dart';
 
 class VideoCard extends StatelessWidget {
   final String thumbnailUrl;
@@ -63,6 +64,43 @@ class VideoCard extends StatelessWidget {
                   const SizedBox(height: 4),
                   Text(timeago.format(uploadedAt),
                       style: Theme.of(context).textTheme.bodySmall),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class VideoCardShimmer extends StatelessWidget {
+  const VideoCardShimmer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: Shimmer.fromColors(
+        baseColor: Colors.grey[300]!,
+        highlightColor: Colors.grey[100]!,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Container(color: Colors.white),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(height: 14, width: double.infinity, color: Colors.white),
+                  const SizedBox(height: 4),
+                  Container(height: 12, width: 80, color: Colors.white),
+                  const SizedBox(height: 8),
+                  Container(height: 10, width: 50, color: Colors.white),
                 ],
               ),
             ),
